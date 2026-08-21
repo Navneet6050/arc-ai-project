9a41a4fab783951fbf0184e4603160bc463aea43
Navneet Kumar <navneet.beast04@gmail.com>
2026-08-21 10:38:41 +0530

    added vercel frontend url

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 // --- 3. Socket.IO Setup with Auth Middleware ---
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5000", // Adjust port if needed
+        origin: "https://arc-ai-project.vercel.app", // Adjust port if needed
         methods: ["GET", "POST"]
     }
 });
